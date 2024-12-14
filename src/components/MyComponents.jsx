ade3529f411f0682da043322c1a8a709b096ac7c
Ammar Ahmad <hammadmalli64@gmail.com>
2024-12-15 00:40:42 +0500

    Counter

--- src/components/MyComponents.jsx
+++ src/components/MyComponents.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 function MyComponenets(){
 
-    const [name, setName] = useState("");
-    const [quantity, setQuantity] = useState();
+    const [name, setName] = useState("User");
+    const [quantity, setQuantity] = useState(0);
     const [comment, setComment] = useState("");
     const [payment, setPayment] = useState("");
     const [shipping, setShipping] = useState("");
@@ -32,8 +32,43 @@ function MyComponenets(){
 
     return (
         <>
-        <input type="text" onChange={(event)=> setUserName(event)}/>
+        <input value={name} onChange={(event)=> setUserName(event)}/>
         <p>Name : {name}</p>
+
+        <input value={quantity} type="number" onChange={(event)=> setTheQUanitity(event)}/>
+        <p>Quantity : {quantity}</p>
+
+        <textarea  
+        placeholder="Enter Delivery Instructions..."
+         onChange={(event)=> setUserComment(event)}
+         />
+        <p>Comment : {comment}</p>
+
+        {/* For DropDown We use Select */}
+        <select value={payment} onChange={(event) => setPaymentMethod(event)}>
+            <option value="">Select Payment Method</option>
+            <option value="Cash">Cash</option>
+            <option value="Cradit Cards">Cradit Cards</option>
+            <option value="Gift Cards">Gift Cards</option>
+            <option value="e-Wallet">e-Wallet</option>
+        </select>
+        <p>Payment Method : {payment}</p>
+
+
+        <label >
+            <input 
+            type="radio"
+             value="Pick-Up"
+              checked={shipping === "Pick-Up"}
+               onChange={setShippingMethod}/>
+            Pick-Up
+        
+        </label>
+        <br />
+        <label >
+            <input type="radio" value="Delivery" checked={shipping === "Delivery"} onChange={setShippingMethod}/>
+            Delivery
+        </label>
         </>
     );
 }
